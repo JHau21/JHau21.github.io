974db8a2bc6e42a1c61e08f580c7c6945e28bc14
JHau21 <JackHau13@gmail.com>
2022-04-10 21:29:28 -0600

    Added a loader and finished assignment styling

--- js/indexQuoteMachine.js
+++ js/indexQuoteMachine.js
@@ -3,30 +3,37 @@ const button = document.getElementById("generateQuoteButton");
 button.addEventListener("click", onClick);
 
 async function onClick() {
-	try {
-		const endpoint =
-			"https://goquotes-api.herokuapp.com/api/v1/random?count=1";
+	const quoteButton = document.getElementById("generateQuoteButton");
+	quoteButton.textContent = "Loading...";
+	// Added setTimeout to prevent spam and allow for loader
+	setTimeout(async () => {
+		try {
+			const endpoint =
+				"https://goquotes-api.herokuapp.com/api/v1/random?count=1";
 
-		const resp = await fetch(endpoint);
+			const resp = await fetch(endpoint);
 
-		if (resp.ok) {
-			const { quotes } = await resp.json();
+			if (resp.ok) {
+				quoteButton.textContent = "Generate Quote";
 
-			handleQuote(quotes[0]);
+				const { quotes } = await resp.json();
 
-			// console.log(json);
-		} else {
-			console.log("Im a fucking dumbass");
+				handleQuote(quotes[0]);
+			} else {
+				console.log("Response Error");
 
-			throw Error(resp.statusText);
+				throw Error(resp.statusText);
+			}
+		} catch (error) {
+			console.log("Try-catch error: ", error);
 		}
-	} catch (error) {
-		console.log("something fucked up with error message: ", error);
-	}
+	}, 1000);
 }
 
 function handleQuote(quote) {
 	const quoteDisplay = document.getElementById("quoteContainerElement");
-	const { text, author, tag } = quote;
+	const quoteAuthor = document.getElementById("quoteAuthorContainer");
+	const { text, author } = quote;
 	quoteDisplay.textContent = text;
+	quoteAuthor.textContent = ": " + author;
 }
