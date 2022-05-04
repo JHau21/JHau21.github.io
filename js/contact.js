9034f8b4377d41f15c803c553be4bacaf4cfbd53
JHau21 <JackHau13@gmail.com>
2022-05-03 18:55:08 -0600

    fixed broken links

---
+++ js/contact.js
@@ -0,0 +1,11 @@
+const urls = [
+	"https://github.com/JHau21",
+	"http://JHau21.github.io/index.html",
+	"http://JHau21.github.io/experience.html",
+	"http://Jhau21.github.io/projects.html",
+	"https://www.linkedin.com/in/john-hau-316450207/",
+];
+
+function navigateTo(n) {
+	window.open(urls[n]);
+}
