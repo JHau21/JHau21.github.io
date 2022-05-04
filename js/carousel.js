3a5363bc442a96691b07a5d74e392d088086cb35
JHau21 <JackHau13@gmail.com>
2022-05-03 18:40:51 -0600

    Finished carousels and minor website changes to address contract

--- js/carousel.js
+++ js/carousel.js
@@ -1,32 +1,70 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let currSlide;
+let numSlides;
+const inSlide = "carouselTextBoxesContainer animationIn";
+const outSlide = "carouselTextBoxesContainer animationOut";
+const progCurr = "progressBox progressBoxActive";
+const progNorm = "progressBox";
+const inImage = "carouselBackgroundImage animationIn";
+const outImage = "carouselBackgroundImage animationOut";
 
-// Next/previous controls
 function onClickArrow(n) {
-	showSlides((slideIndex += n));
+	prevSlide = currSlide;
+	currSlide += n;
+
+	if (currSlide > numSlides - 1) {
+		currSlide = 0;
+	} else if (currSlide < 0) {
+		currSlide = numSlides - 1;
+	}
+
+	transition(prevSlide, currSlide);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-	showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-	let i;
+function transition(prev, curr) {
 	let slides = document.getElementsByClassName("carouselTextBoxesContainer");
-	// let dots = document.getElementsByClassName("dot");
-	if (n > slides.length) {
-		slideIndex = 1;
-	}
-	if (n < 1) {
-		slideIndex = slides.length;
-	}
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	// for (i = 0; i < dots.length; i++) {
-	// 	dots[i].className = dots[i].className.replace(" active", "");
-	// }
-	slides[slideIndex - 1].style.display = "block";
-	// dots[slideIndex - 1].className += " active";
+	let slideBar = document.getElementsByClassName("progressBox");
+	let background = document.getElementsByClassName(
+		"carouselBackgroundImage"
+	);
+
+	slideBar[prev].className = progNorm;
+	slideBar[curr].className = progCurr;
+
+	let oldSlide = slides[prev];
+	let newSlide = slides[curr];
+	let oldImage = background[prev];
+	let newImage = background[curr];
+
+	oldSlide.className = outSlide;
+	newSlide.className = inSlide;
+	oldImage.className = outImage;
+	newImage.className = inImage;
+	newImage.style.display = "block";
+	newSlide.style.display = "block";
+
+	setTimeout(() => {
+		oldSlide.style.display = "none";
+		oldImage.style.display = "none";
+	}, 1000);
 }
+
+// Load in the first slide
+window.onload = function () {
+	let slides = document.getElementsByClassName("carouselTextBoxesContainer");
+	let slideBar = document.getElementsByClassName("progressBox");
+	let background = document.getElementsByClassName(
+		"carouselBackgroundImage"
+	);
+
+	numSlides = slides.length;
+
+	slides[0].className = "carouselTextBoxesContainer";
+	slides[0].style.display = "block";
+
+	slideBar[0].className = "progressBox progressBoxActive";
+
+	background[0].className = "carouselBackgroundImage";
+	background[0].style.display = "block";
+
+	currSlide = 0;
+};
